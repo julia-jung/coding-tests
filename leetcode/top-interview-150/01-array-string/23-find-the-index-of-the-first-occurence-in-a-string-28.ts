a863be9a929753aa7266851fb499918782882b3a
julia8956 <minji6654@gmail.com>
2023-12-12 13:37:30 +0900

    day 12 extra

---
+++ leetcode/top-interview-150/01-array-string/23-find-the-index-of-the-first-occurence-in-a-string-28.ts
@@ -0,0 +1,25 @@
+/** ⭐
+ * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, 
+ * or -1 if needle is not part of haystack.
+ */
+
+// Solution 1: looping string
+function strStr(haystack: string, needle: string): number {
+  for (let i = 0 ; i < haystack.length; i++) {
+    if (haystack[i] === needle[0]) {
+      let j = 0;
+      while (j < needle.length && haystack[i + j] === needle[j]) j++;
+      if (j === needle.length) return i;
+    }
+  }
+  
+  return -1;
+};
+
+strStr('sadbutsad', 'sad'); // 0
+strStr('leetcode', 'leeto'); // -1
+
+// Solution 2: using built-in String method
+function strStr2(haystack: string, needle: string): number {
+  return haystack.indexOf(needle);
+};
