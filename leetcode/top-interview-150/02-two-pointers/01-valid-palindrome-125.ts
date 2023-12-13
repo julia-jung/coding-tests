37f31c996ee389f1127af4079270eb1c0fb512ee
julia8956 <minji6654@gmail.com>
2023-12-13 12:47:23 +0900

    day 13

---
+++ leetcode/top-interview-150/02-two-pointers/01-valid-palindrome-125.ts
@@ -0,0 +1,41 @@
+/** ⭐
+ * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, 
+ * it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ */
+
+// Solution 1: replacing with RegExp
+function isPalindrome(s: string): boolean {
+  const convertedStr = s.toLowerCase().replace(/[^0-9a-z]/gi, '');
+
+  let left = 0;
+  let right = convertedStr.length - 1;
+
+  while (left <= right) {
+    if (convertedStr[left++] !== convertedStr[right--]) return false;
+  }
+  return true;
+};
+
+
+// Solution: without RegExp 
+function isPalindrome2(s: string): boolean {
+  s = s.toLowerCase();
+  let l = 0;
+  let r = s.length - 1;
+
+  while (l < r) {
+    while (!isAlphaNumeric(s[l])) l++;
+    while (!isAlphaNumeric(s[r])) r--;
+
+    if (s[l++] !== s[r--]) {
+      return false;
+    }
+  }
+
+  return true;
+  
+  function isAlphaNumeric(c: string): boolean {
+    return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+  }
+};
---
+++ leetcode/top-interview-150/02-two-pointers/02-is-subsequence-392.ts
@@ -0,0 +1,37 @@
+/** ⭐
+ * Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+ * A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters 
+ * without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+ */
+
+// using two pointers for each string
+// Solution 1: iterating "t", move pointer of "s" if condition is satisfied
+function isSubsequence(s: string, t: string): boolean {
+  if (s === t) return true;
+  if (s.length > t.length) return false;
+
+  let i = 0;
+  let j = 0;
+
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
+  }
+
+  return i === s.length;
+};
+
+// Solution 2: iterating "s", move pointer of "t" if condition is not satisfied
+function isSubsequence2(s: string, t: string): boolean {
+  let i = 0;
+  let j = 0;
+
+  while (i < s.length) {
+    while (j < t.length && t[j] !== s[i]) j++;
+    if (j >= t.length) return false;
+    i++;
+    j++
+  }
+
+  return true;
+};
