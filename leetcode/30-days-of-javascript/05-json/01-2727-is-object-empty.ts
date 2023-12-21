6ebf645a68b96db6da091edf95d398aaae0dfa95
julia8956 <minji6654@gmail.com>
2023-12-21 18:14:42 +0900

    day 21

--- leetcode/30-days-of-javascript/05-json/01-2727-is-object-empty.ts
+++ leetcode/30-days-of-javascript/05-json/01-2727-is-object-empty.ts
@@ -1,4 +1,4 @@
-/**
+/** ⭐
  * Given an object or an array, return if it is empty.
  * An empty object contains no key-value pairs.
  * An empty array contains no elements.
---
+++ leetcode/top-interview-150/09-binary-tree-general/13-222-count-complete-tree-nodes.ts
@@ -0,0 +1,46 @@
+/** ⭐
+ * Given the root of a complete binary tree, return the number of the nodes in the tree.
+ * According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+ * Design an algorithm that runs in less than O(n) time complexity.
+ */
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+  }
+}
+
+// Solution 1: count all from left to right
+function countNodes(root: TreeNode | null): number {
+  if (!root) return 0;
+  return 1 + countNodes(root.left) + countNodes(root.right);
+};
+
+// Solution 2: compare left, right subtree heights to decide if they are full binary tree
+function countNodes2(root: TreeNode | null): number {
+  const height = getHeight(root);
+  if (height <= 1) return height;
+
+  let leftCount = 0;
+  let rightCount = 0;
+  if (getHeight(root.right) === height - 1) {
+    // If right-subtree has the same height with left-subtree
+    leftCount = 2 ** (height - 1) - 1; // left-subtree is a full-tree of height "fullHeight - 1"
+    rightCount = countNodes(root.right); // right-subtree count can get recursively
+  } else {
+    // means, last nodes on the last tree row is in the left-subtree
+    leftCount = countNodes(root.left); // count of left-subtree can get recursively
+    rightCount = 2 ** (height - 2) - 1; // right-subtree is a full-tree of height "fullHeight - 2"
+  }
+  
+  return 1 + leftCount + rightCount;
+
+  function getHeight(root: TreeNode | null): number {
+    return root ? 1 + getHeight(root.left) : 0;
+  }
+};
