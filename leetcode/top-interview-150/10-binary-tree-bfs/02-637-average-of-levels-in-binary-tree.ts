e3c1e2a77013cf142b0566827d111c4770d55820
julia8956 <minji6654@gmail.com>
2023-12-22 23:50:33 +0900

    day 22

---
+++ leetcode/top-interview-150/10-binary-tree-bfs/02-637-average-of-levels-in-binary-tree.ts
@@ -0,0 +1,63 @@
+/** ⭐
+ * Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. 
+ * Answers within 10-5 of the actual answer will be accepted.
+ */
+
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+  }
+}
+
+// Solution 1: push nodes into queue iterating each level
+function averageOfLevels(root: TreeNode | null): number[] {
+  const result = [];
+  if (!root) return result;
+
+  const queue = [root];
+  
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    let levelSum = 0;
+    
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift(); // polling first node from the queue
+      levelSum += node.val;
+      
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    result.push(levelSum / levelSize);
+  }
+  return result;
+};
+
+// Solution 2: convert a whole tree into number array then calculate avg for each level
+function averageOfLevels2(root: TreeNode | null): number[] {
+  if (!root) return [];
+
+  let i = 0;
+  const arr: number[][] = [];
+  pushVal(i, root);
+
+  return arr.map((vals) => {
+    const sum = vals.reduce((acc, cur) => acc + cur, 0);
+    const count = vals.length;
+    return Number((sum / count).toFixed(5));
+  });
+
+  function pushVal(i: number, node: TreeNode): void {
+    if (!node) return;
+    arr[i] ? arr[i].push(node.val) : arr[i] = [node.val];
+    
+    pushVal(i + 1, node.left);
+    pushVal(i + 1, node.right);
+  }
+};
