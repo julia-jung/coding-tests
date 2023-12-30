0f1762f6eb96732027824fc74254aa419abbef37
julia8956 <minji6654@gmail.com>
2023-12-30 16:04:29 +0900

    day 30. end of JS and easy problems of interviews150

---
+++ leetcode/top-interview-150/22-1d-dp/01-70-climbing-stairs.ts
@@ -0,0 +1,77 @@
+/** ⭐
+ * You are climbing a staircase. It takes n steps to reach the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ */
+
+
+// Solution 1: using array of fibonnaci numbers
+function climbStairs(n: number): number {
+  // each item represents the distinct way to get to the each steps (0, first, second, ..., nth)
+  const steps: number[] = [1, 2]; // only one way to get the first step, two ways to get to the second step
+
+  for (let i = 2; i < n; i++) {
+    steps[i] = steps[i - 1] + steps[i - 2];
+    // [1, 2, 3, 5, 8, 13, ...] -> fibonacci numbers
+  }
+
+  return steps[n - 1];
+};; 
+
+climbStairs(3); // 3
+climbStairs(4); // 5
+climbStairs(5); // 8
+
+// Solution 3: space optimized only using two variables instead using the whole array
+function climbStairs2(n: number): number {
+  if (n <= 2) return n;
+  let prev = 1;
+  let curr = 2;
+
+  for (let i = 2; i < n; i++) {
+    const temp = curr;
+    curr = prev + curr;
+    prev = temp;
+  }
+
+  return curr;
+};
+
+
+// Failed: Time limit exceeded
+function climbStairsFailed(n: number): number {
+  let result = 0;
+  takeSteps(n);
+  
+  function takeSteps(n: number) {
+    if (n < 2) {
+      result++;
+    } else {
+      takeSteps(n - 1);
+      takeSteps(n - 2);
+    }
+
+  }
+  return result;
+};
+
+// Failed: Time limit exceeded
+// this solution has exponential time complexity (O(2^n)) due to redundant calculations.
+function climbStairsFailed2(n: number): number {
+  if (n < 2) return 1;
+  return climbStairsFailed2(n - 1) + climbStairsFailed2(n - 2);
+};
+
+// Solution 3: avoid redundant calculation memoizing(using Map)
+function climbStairs3(n: number): number {
+  const memo: Map<number, number> = new Map();
+  return climb(n);
+  
+  function climb(n: number): number {
+    if (n < 2) return 1;
+    if (!memo.has(n)) {
+      memo.set(n, climb(n - 1) + climb(n - 2));
+    }
+    
+    return memo.get(n);
+  }
+};
